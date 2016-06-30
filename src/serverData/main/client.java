81797295044af89f71aa305667330125815f9b5e
SpyGuyIan <spyguyian@gmail.com>
2016-06-30 12:14:33 -0400

    add Data Streams

--- src/serverData/main/client.java
+++ src/serverData/main/client.java
@@ -1,28 +1,54 @@
 package serverData.main;
 
+import java.io.*;
 import java.io.IOException;
 import java.net.*;
 
 public class client {
-	Socket s;
-	public client() {
-		System.out.println("Client init");
-	}
+    Socket s;
+    DataOutputStream dos;
+    DataInputStream dis;
 
-	public void init(String ip, int port){
-		try {
-			s = new Socket(ip, port);
-			System.out.println("Connected");
-			Game ex = new Game();
-			ex.setVisible(true);
-			System.out.println("working");
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-			System.out.println("Unknown Host");
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.println("IOExeption error");
-		}
-	}
 
+    public client() {
+	System.out.println("Client init");
+    }
+
+    public void init(String ip, int port) {
+	try {
+	    s = new Socket(ip, port);
+	    System.out.println("Connected");
+
+	    dos = new DataOutputStream(s.getOutputStream());
+	    dis = new DataInputStream(s.getInputStream());
+	    System.out.println("Data Streams created");
+	    
+	    Game ex = new Game();
+	    ex.setVisible(true);
+
+	} catch (UnknownHostException e) {
+	    e.printStackTrace();
+	    System.out.println("Unknown Host");
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.out.println("IOExeption error");
+	}
+    }
+
+    public void sendString(String output) {
+	try {
+	    dos.writeUTF(output);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public String readString() {
+	try {
+	    return dis.readUTF();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    return "IOException";
+	}
+    }
 }
--- src/serverData/main/server.java
+++ src/serverData/main/server.java
@@ -1,27 +1,51 @@
 package serverData.main;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.*;
 
 public class server {
+    DataInputStream dis;
+    DataOutputStream dos;
+    ServerSocket server;
+    Socket s;
 
-	ServerSocket server;
-	Socket s;
+    public server() {
+	System.out.println("Server init");
 
-	public server() {
-		System.out.println("Server init");
+    }
 
+    public void init(int port) {
+	try {
+	    server = new ServerSocket(port);// may not be port just an id number
+	    s = server.accept();
+
+	    dos = new DataOutputStream(s.getOutputStream());
+	    dis = new DataInputStream(s.getInputStream());
+	    System.out.println("Data Streams created");
+
+	    System.out.println("Connected");
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.out.println("IOExeption error---");
 	}
+    }
 
-
-	public void init(int port) {
-		try {
-			server = new ServerSocket(port);// may not be port just an id number
-			s = server.accept();
-			System.out.println("Connected");
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.println("IOExeption error---");
-		}
+    public void sendString(String output) {
+	try {
+	    dos.writeUTF(output);
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
+
+    public String readString(){
+	try {
+	    return dis.readUTF();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    return "IOException";
+	}
+    }
 }
